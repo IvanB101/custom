8ab03c3572f95b991c0ddac2e18279ef7282aced
IvanB101 <ivanbrocas01@gmail.com>
2023-04-27 15:01:59 -0300

    first commit

---
+++ plugins.lua
@@ -0,0 +1,32 @@
+local plugins = {
+  {
+    "vim-crystal/vim-crystal",
+    ft = "crystal",
+    config = function (_)
+      vim.g.crystal_auto_format = 1
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "bash-language-server",
+        "clangd",
+        "cmakelang",
+        "jdtls",
+        "json-lsp",
+        "lua-language-server",
+        "beautysh",
+      }
+    }
+  },
+}
+return plugins
